1d9d7fa97ba1763817dd02206e84bdee5a42b078
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2019-09-17 11:56:46 -0300

    new version released, with new colunm 'quantidade de tickets'

--- count_and_amount_of_hours.js
+++ count_and_amount_of_hours.js
@@ -103,7 +103,7 @@ workbook.xlsx.readFile(SOURCE_FILE)
         var months_years = []
         var years = []
         var titles = []
-
+        worksheet.getCell(STORE_QUANTIDADE_TICKETS + 1).value = "Quantidade de tickets"
         while (i <= worksheet.rowCount) {
 
             // var assignee = worksheet.getCell(CARD_ASSIGNEES + i).value
