470f95862d503ddae6d83a3baa05f7b38c8f817d
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2019-10-25 16:22:49 -0300

    new dates

---
+++ inverter_datas.js
@@ -0,0 +1,90 @@
+require('dotenv').config({ path: 'config' })
+const utf8 = require('utf8');
+
+var Excel = require('exceljs');
+var workbook = new Excel.Workbook();
+var not_allowed = [];
+
+// CONTROLLERS
+const LABELS_COLUMN = process.env.LABELS_COLUMN
+
+const SOURCE_FILE = process.env.SOURCE_FILE
+const OUTPUT_FILE = process.env.OUTPUT_FILE
+const WORKSHEET = process.env.WORKSHEET
+const CREATED_BY_COLUMN = process.env.CREATED_BY_COLUMN
+const TITLE_COLUMN = process.env.TITLE_COLUMN
+const PRIMARY_LABELS_COLUMN = process.env.PRIMARY_LABELS_COLUMN
+const CLOSED_AT = process.env.CLOSED_AT
+const CLIENTS_COLUMN = process.env.CLIENTS_COLUMN
+const CREATED_AT = process.env.CREATED_AT
+
+const STORE_CREATED_BY_COLUMN = process.env.STORE_CREATED_BY_COLUMN
+const STORE_SHIFT = process.env.STORE_SHIFT
+const STORE_CLIENT_COLUMN = process.env.STORE_CLIENT_COLUMN
+const STORE_TITLE_COLUMN = process.env.STORE_TITLE_COLUMN
+const STORE_PRIMARY_LABELS_COLUMN = process.env.STORE_PRIMARY_LABELS_COLUMN
+const STORE_TYPE_COLUMN = process.env.STORE_TYPE_COLUMN
+const STORE_SEVERITY_COLUNM = process.env.STORE_SEVERITY_COLUNM
+const STORE_CLOSED_AT = process.env.STORE_CLOSED_AT
+const STORE_WEEK_DAY = process.env.STORE_WEEK_DAY
+const STORE_MONTH = process.env.STORE_MONTH
+const STORE_DAY = process.env.STORE_DAY
+const STORE_YEAR = process.env.STORE_YEAR
+
+const SEV_SUMMARY_LABELS = process.env.SEV_SUMMARY_LABELS
+const SEV_SUMMARY_VALUES = process.env.SEV_SUMMARY_VALUES
+const SEV_SUMMARY_CLIENT_NAME = process.env.SEV_SUMMARY_CLIENT_NAME
+const SEV_SUMMARY_CLIENT_SEV1 = process.env.SEV_SUMMARY_CLIENT_SEV1
+const SEV_SUMMARY_CLIENT_SEV2 = process.env.SEV_SUMMARY_CLIENT_SEV2
+const SEV_SUMMARY_CLIENT_SEV3 = process.env.SEV_SUMMARY_CLIENT_SEV3
+const SEV_SUMMARY_CLIENT_SEV4 = process.env.SEV_SUMMARY_CLIENT_SEV4
+const SOURCE_COLUMNS_LIST = process.env.SOURCE_COLUMNS_LIST
+const DESTINATION_COLUMNS_LIST = process.env.DESTINATION_COLUMNS_LIST
+
+
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+
+// READ WORKBOOK
+workbook.xlsx.readFile(SOURCE_FILE)
+    .then(function () {
+        var worksheet = workbook.getWorksheet(WORKSHEET);
+        var i = 2;
+
+        //setting the title of the column
+        // worksheet.getCell(STORE_WEEK_DAY + 1).value = "week_day"
+        // worksheet.getCell(STORE_DAY + 1).value = "Dia"
+        // worksheet.getCell(STORE_YEAR + 1).value = "Ano"
+
+        while (i <= worksheet.rowCount) {
+            var cell_closed_at = worksheet.getCell(STORE_CLOSED_AT + i).value
+            var cell_created_at = worksheet.getCell(CREATED_AT + i).value
+            if (cell_closed_at != null) {
+                var partes = cell_closed_at.split(" ");
+                // var date = new Date(partes[0])
+                var partes_partes = partes[0].split("/")
+                var dataAlterada = partes_partes[1] + "/" + partes_partes[0] + "/" + partes_partes[2]
+                worksheet.getCell(STORE_CLOSED_AT + i).value = dataAlterada + " " + partes[1]
+            }
+
+            if (cell_created_at != null) {
+                var partes = cell_created_at.split(" ");
+                // var date = new Date(partes[0])
+                var partes_partes = partes[0].split("/")
+                var dataAlterada = partes_partes[1] + "/" + partes_partes[0] + "/" + partes_partes[2]
+                worksheet.getCell(CREATED_AT + i).value = dataAlterada + " " + partes[1]
+            }
+            // var valor_celula = worksheet.getCell(CREATED_AT + i).value
+            // if (valor_celula != null) {
+            //     var pieces = valor_celula.split(" ")
+            //     var date = new Date(pieces[0])
+            //     var dayName = days[date.getDay()];
+            //     worksheet.getCell(STORE_WEEK_DAY + i).value = dayName
+            //     worksheet.getCell(STORE_DAY + i).value = date.getDay().toString().padStart(2, '0')
+            //     worksheet.getCell(STORE_YEAR + i).value = date.getFullYear()
+            // }
+            i++;
+        }
+        console.log('finalizado!');
+        return workbook.xlsx.writeFile(OUTPUT_FILE);
+    })
