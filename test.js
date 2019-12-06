281dcf16bb7e3eda94f941cbca72856d558bbfca
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2019-12-05 23:12:50 -0300

    ordem das colunas mudada

--- test.js
+++ test.js
@@ -2,6 +2,10 @@ var fs = require('fs');
 
 var contents = fs.readFileSync("a.csv", { encoding: 'utf8' });
 
-console.log(contents.toString().split("\n")[941].toString().split(",").length)
-console.log(contents.toString().split("\n")[942].toString().split(",").length)
-console.log(contents.toString().split("\n")[943].toString().split(",").length)
+console.log(contents.toString().split("\n")[32].toString())
+
+
+// console.log(contents.toString().split("\n")[941].toString().split(",").length)
+// console.log(contents.toString().split("\n")[942].toString().split(",").length)
+// console.log(contents.toString().split("\n")[943].toString().split(",").length)
+
