cdee3eef31f12d4bcdc69715a309cf3983eb4ebc
Bruno Eleodoro Roza <Bruno.Eleodoro.Roza@ibm.com>
2020-06-27 10:55:09 -0300

    Analise

--- preencher_nao_informado.js
+++ preencher_nao_informado.js
@@ -29,24 +29,34 @@ workbook.xlsx.readFile(config.SOURCE_FILE)
             worksheet.getCell(config.PROBLEMA_REPORTADO + i).value = verifyNa(worksheet, config.PROBLEMA_REPORTADO + i)
             worksheet.getCell(config.CATEGORIA + i).value = verifyNa(worksheet, config.CATEGORIA + i)
             worksheet.getCell(config.SERVICE_LINE + i).value = verifyNa(worksheet, config.SERVICE_LINE + i)
-            worksheet.getCell(config.HORARIO_INCIDENTE + i).value = verifyNa(worksheet, config.HORARIO_INCIDENTE + i)
-            worksheet.getCell(config.SLA_TICKET + i).value = verifyNa(worksheet, config.SLA_TICKET + i)
-            worksheet.getCell(config.HORARIO_ACIONAMENTO + i).value = verifyNa(worksheet, config.HORARIO_ACIONAMENTO + i)
-            worksheet.getCell(config.ISM_SOLICITOU + i).value = verifyNa(worksheet, config.ISM_SOLICITOU + i)
             worksheet.getCell(config.TRIBE + i).value = verifyNa(worksheet, config.TRIBE + i)
 
             var severidade = worksheet.getCell(config.STORE_SEVERITY_COLUNM + i).value
             var type = worksheet.getCell(config.STORE_TYPE_COLUMN + i).value
+            var label = "";
 
             if (severidade == "N/A") {
                 if (type == "CH") {
-                    worksheet.getCell(config.STORE_SEVERITY_COLUNM + i).value = "N/A - CHANGE"
+                    label = "N/A - CHANGE"
                 } else if (type == "REPORT") {
-                    worksheet.getCell(config.STORE_SEVERITY_COLUNM + i).value = "N/A - REPORT"
+                    label = "N/A - REPORT"
                 } else if (type == "SC") {
-                    worksheet.getCell(config.STORE_SEVERITY_COLUNM + i).value = "N/A - SEM CHAMADO"
+                    label = "N/A - SEM CHAMADO"
                 }
             }
+            worksheet.getCell(config.STORE_SEVERITY_COLUNM + i).value = label
+            worksheet.getCell(config.HORARIO_INCIDENTE + i).value = label
+            worksheet.getCell(config.SLA_TICKET + i).value = label
+            worksheet.getCell(config.HORARIO_ACIONAMENTO + i).value = label
+            worksheet.getCell(config.ISM_SOLICITOU + i).value = label
+            worksheet.getCell(config.TEMPO_ATENDIMENTO + i).value = label
+            worksheet.getCell(config.ANALISE_PRAZO_ACIONAMENTO + i).value = label
+            // Horário do Incident,
+            // SLA do ticket
+            // Horário Acionamento ISM
+            // ISM solicitou Validação
+            // Tempo de Atendimento ISM
+            // Análise do Prazo de Acionamento ISM
             i++;
         }
 
