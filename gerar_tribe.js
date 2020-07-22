24de33eead4336fb500ff5ab18ecb41ab0907195
Bruno Eleodoro Roza <Bruno.Eleodoro.Roza@ibm.com>
2020-07-22 20:11:41 -0300

    new changes

--- gerar_tribe.js
+++ gerar_tribe.js
@@ -1,10 +1,8 @@
 require('dotenv').config({ path: 'config' })
 const utf8 = require('utf8');
-
 var Excel = require('exceljs');
 var workbook = new Excel.Workbook();
 var not_allowed = [];
-
 // CONTROLLERS
 const STORE_PRIMARY_LABELS_COLUMN = process.env.STORE_PRIMARY_LABELS_COLUMN
 const STORE_TYPE_COLUMN = process.env.STORE_TYPE_COLUMN
@@ -16,113 +14,109 @@ const OUTPUT_FILE = process.env.OUTPUT_FILE
 const WORKSHEET = process.env.WORKSHEET
 const STORE_SEVERITY_COLUNM = process.env.STORE_SEVERITY_COLUNM
 const TRIBE = process.env.TRIBE
-
 const TOTAL_WAITING_TIME = process.env.TOTAL_WAITING_TIME
 const OPERATIONAL_LEAD_TIME = process.env.OPERATIONAL_LEAD_TIME
 const STORE_QUANTIDADE_TICKETS = process.env.STORE_QUANTIDADE_TICKETS
 const STORE_DAY = process.env.STORE_DAY
 const STORE_YEAR = process.env.STORE_YEAR
 const QUANTIDADE_TICKETS_PER_USER = process.env.QUANTIDADE_TICKETS_PER_USER
-
 //Summary for severities
 const SEV_SUMMARY_LABELS = process.env.SEV_SUMMARY_LABELS
 const SEV_SUMMARY_VALUES = process.env.SEV_SUMMARY_VALUES
-
 // Summary for severities of clients
 const SEV_SUMMARY_CLIENT_NAME = process.env.SEV_SUMMARY_CLIENT_NAME
 const SEV_SUMMARY_CLIENT_SEV1 = process.env.SEV_SUMMARY_CLIENT_SEV1
 const SEV_SUMMARY_CLIENT_SEV2 = process.env.SEV_SUMMARY_CLIENT_SEV2
 const SEV_SUMMARY_CLIENT_SEV3 = process.env.SEV_SUMMARY_CLIENT_SEV3
 const SEV_SUMMARY_CLIENT_SEV4 = process.env.SEV_SUMMARY_CLIENT_SEV4
-
 var pessoas_clientes = []
-var pessoas = ["Daniella Yumi Itikawa",
+var pessoas = ["Marcia Y Couto",
     "Edmar Lauria Marques",
     "Geraldo Vicente Ferreira",
-    "Marcelo Mendes Genaro",
+    "Fabiano Gonçalves",
     "Paulinho Rossetti",
-    "Lívio Teixeira"
+    "Multiplos Clientes",
+    "Lívio Teixeira",
+    "IBM Infra",
 ]
-
-pessoas_clientes["Daniella Yumi Itikawa"] = [
-    "FLEURY",
-    "MULTIPLUS",
-    "SAINT GOBAIN",
-    "BRF",
-    "CARREFOUR",
+pessoas_clientes["Marcia Y Couto"] = [
+    "ADP",
+    "AREZZO",
+    "ARTERIS",
+    "BANCO PINE",
+    "BCG",
+    "BURGUER KING",
+    "CEBRACE",
     "CONSTRUDECOR",
-    "UNILEVER",
+    "CRESOL",
+    "FLEURY",
+    "GRUPO SIMOES",
+    "LASA",
+    "MULTIPLUS",
+    "PROXXI",
+    "RIO GALEAO",
+    "SAINT GOBAIN",
+    "SENIOR",
+    "TICKET",
+    "TOYOTA",
+    "ZETRASOFT",
 ]
 pessoas_clientes["Lívio Teixeira"] = [
-    "CAIXA ECONOMICA",
+    "CAIXA ECONOMICA"
+]
+pessoas_clientes["Multiplos Clientes"] = [
+    "MULTIPLOS CLIENTES"
+]
+pessoas_clientes["IBM Infra"] = [
+    "IRM"
 ]
 pessoas_clientes["Edmar Lauria Marques"] = [
+    "BANRISUL",
     "BOA VISTA",
-    "BURGUER KING",
-    "BURGER KING",
+    "BRF",
     "FIAT",
     "GERDAU",
     "HONDA",
-    "INTERMEDICA",
     "MERCEDES",
-    "MERCEDES BENZ",
     "PESA",
     "RIOCARD",
-    "BANRISUL"
+    "UNILEVER"
 ]
-pessoas_clientes["Geraldo Vicente Ferreira"] = [
+pessoas_clientes["Fabiano Gonçalves"] = [
     "ALPARGATAS",
     "APOLLO",
     "BR MALLS",
-    "BRMALLS",
-    "COPERSUCAR",
-    "DROGARIA SP",
-    "GPA",
-    "ONOFRE",
-    "VIA VAREJO",
-]
-pessoas_clientes["Marcelo Mendes Genaro"] = [
-    "ANBIMA",
-    "ARTERIS",
+    "CARREFOUR",
     "CMOC",
+    "COPERSUCAR",
     "CRDC",
+    "DROGARIA SP",
     "ESSILOR",
     "ETERNIT",
-    "GENERALI",
-    "LASA",
+    "ANBIMA",
+    "GENERALLI",
     "LEAO",
     "LEROY MERLIN",
     "MANGELS",
+    "GPA",
     "RECORD",
     "REDECARD",
     "SANTA HELENA",
     "SPRINGER",
     "TIGRE",
+    "VIA VAREJO",
+]
+pessoas_clientes["Geraldo Vicente Ferreira"] = [
+    "SANTANDER",
 ]
 pessoas_clientes["Paulinho Rossetti"] = [
-    "ADP",
-    "AREZZO",
-    "BANCO PINE",
-    "BANCO TOYOTA",
-    "TOYOTA",
-    "BCG",
     "BRADESCO",
-    "CEBRACE",
+    "BRADESCO MSA",
     "CIELO",
-    "CRESOL",
-    "FIRST DATA",
+    "ELO",
     "LIVELO",
-    "NIDERA",
-    "SENIOR",
-    "STELO",
-    "ZETRASOFT",
-    "PROXXI",
-    "RIOGALEAO",
-    "GRUPO SIMOES",
-    "IRM"
+    "STELO"
 ]
-
-
 // READ WORKBOOK
 workbook.xlsx.readFile(SOURCE_FILE)
     .then(function () {
@@ -130,7 +124,6 @@ workbook.xlsx.readFile(SOURCE_FILE)
         var i = 2;
         worksheet.getCell(TRIBE + 1).value = "Tribo"
         while (i <= worksheet.rowCount) {
-
             var k = 0;
             while (k < pessoas.length) {
                 var value = worksheet.getCell(CLIENTS_COLUMN + i).value
@@ -140,7 +133,6 @@ workbook.xlsx.readFile(SOURCE_FILE)
                     console.log(pessoas_clientes[pessoa].indexOf(value))
                 }
                 var pessoa = pessoas[k]
-
                 if (pessoas_clientes[pessoa].indexOf(value) >= 0) {
                     worksheet.getCell(TRIBE + i).value = pessoa
                     break;
@@ -153,7 +145,6 @@ workbook.xlsx.readFile(SOURCE_FILE)
             //     worksheet.getCell(STORE_DAY + i).value = parseFloat(worksheet.getCell(STORE_DAY + i).value)
             //     worksheet.getCell(STORE_YEAR + i).value = parseFloat(worksheet.getCell(STORE_YEAR + i).value)
             //     worksheet.getCell(QUANTIDADE_TICKETS_PER_USER + i).value = parseFloat(worksheet.getCell(QUANTIDADE_TICKETS_PER_USER + i).value)
-
             i++;
         }
         console.log('finalizado!');
